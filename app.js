ca0e55758e1ec649941a55bd683cdfaaed34d495
Hack Reactor Student <student@hackreactor.com>
2017-08-30 12:32:51 -0500

    all req's met except state functionality

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // TODO
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li>{this.props.item}</li>
+    )
+  }
+}
+
+var groceryItems = ['ham', 'eggs', 'tofu', 'bACONE'];
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <GroceryList items={groceryItems}/>
+  </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
